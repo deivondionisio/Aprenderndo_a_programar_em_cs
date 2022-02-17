14e49e4d476070e9f60debb230b795d228031096
David Dionísio <49300687+deivondionisio@users.noreply.github.com>
2022-02-17 16:01:29 -0300

    Add files via upload

---
+++ Função_Foreach.cs
@@ -0,0 +1,23 @@
+using System;
+   
+ namespace Repetindo_Operacoes_Com_ForEach
+ {
+   class Program
+   {
+    static void Main(string[] args)
+    {
+       string[] nomes = { "Paulo", "Ana", "Felipe", "Laura" };
+            
+         int x = 0;
+            
+                   
+            foreach (string nome in nomes)
+            {
+             	 x++; 
+ 			 	  Console.WriteLine($"O nome do {x}º aluno é {nome}.");
+            }
+
+            Console.ReadLine(); //O ReadLine é apenas p/ parar o console e verificar o resultado
+        }
+    }
+}
