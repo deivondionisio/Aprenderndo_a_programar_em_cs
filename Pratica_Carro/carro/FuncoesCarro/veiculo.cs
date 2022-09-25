0a322aa5f65b159cf13d2aded3a1ecda8e856f20
David Dionísio <49300687+deivondionisio@users.noreply.github.com>
2022-09-24 23:30:11 -0300

    adicionando veiculo

---
+++ Pratica_Carro/carro/FuncoesCarro/veiculo.cs
@@ -0,0 +1,11 @@
+namespace carro.FuncoesCarro
+{
+    public class Veiculo
+    {
+        public int NivelCombustivel { get; set; }
+        public bool Ligado { get; set; }
+        public void Ligar() => Ligado = true;
+        public void Desligar() => Ligado = false;
+        public string Buzinar() => Ligado;
+    }
+}
---
+++ Pratica_Carro/carro/Program.cs
@@ -0,0 +1,35 @@
+﻿using carro.FuncoesCarro;
+
+var moto1 = new Veiculo();
+var carro1 = new Veiculo();
+
+moto1.Ligar();
+
+Console.WriteLine("Limpar moto1");
+LimparVeiculo(moto1);
+
+Console.WriteLine("Limpar carro1");
+LimparVeiculo(carro1);
+
+Console.WriteLine("Buzinar moto1");
+ApertarBuzina(moto1);
+
+Console.WriteLine("Buzinar carro1");
+ApertarBuzina(carro1);
+
+void LimparVeiculo(Veiculo veiculo)
+{
+    if (veiculo.Ligado)
+    {
+        Console.WriteLine("Por favor desligue o veículo");
+    }
+    else
+    {
+        Console.WriteLine("Iniciando limpeza");
+    }
+
+    void ApertarBuzina(Veiculo veiculo)
+    {
+        Console.WriteLine($"Veículo buzinando: {veiculo.Buzinar()}");
+    }
+}
