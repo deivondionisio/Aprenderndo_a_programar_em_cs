b631f71693e1f3cba087c053ae3d3ca20ffa820c
David Dionísio <49300687+deivondionisio@users.noreply.github.com>
2023-04-29 15:49:38 +0000

    tipo inteiro

--- TiposDeValor/IntegralComSinal/TipoInt/Program.cs
+++ TiposDeValor/IntegralComSinal/TipoInt/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Os tipos numéricos integrais representam números inteiros. Todos os tipos numéricos integrais são tipos de valor.
+Eles também são tipos simples e podem ser inicializados com literais.
+Todos os tipos numéricos integrais dão suporte a operadores aritméticos, bit a bit lógicos, de comparação e igualdade.
+*/
+
+
+int numeral = 12;
+
+Console.WriteLine(numeral);
+
+int a = 20;
+int b = 2;
+
+int c = a / b;
+
+Console.WriteLine(c);
