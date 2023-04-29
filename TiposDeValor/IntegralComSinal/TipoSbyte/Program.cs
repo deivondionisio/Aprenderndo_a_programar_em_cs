7a03e96c47f3655d80888d01a9d368b252b6d8d0
David Dionísio <49300687+deivondionisio@users.noreply.github.com>
2023-04-29 15:39:05 +0000

    finalizado sbyte

--- TiposDeValor/IntegralComSinal/TipoSbyte/Program.cs
+++ TiposDeValor/IntegralComSinal/TipoSbyte/Program.cs
@@ -4,3 +4,14 @@ sbyte num1 = 10;// 8 bits, de -128 a 127
 
 Console.WriteLine(num1);
 
+sbyte num2 = -128;
+
+Console.WriteLine(num2);
+
+sbyte num3 = 127;
+
+Console.WriteLine(num3);
+
+//Use o tipo de dados SByte para conter valores inteiros que não exigem a largura completa dos dados de Integer ou mesmo a metade da largura de dados de Short. Em alguns casos, o Common Language Runtime pode ser capaz de agrupar as variáveis SByte e economizar o consumo de memória.
+
+//O valor padrão de SByte é 0.
